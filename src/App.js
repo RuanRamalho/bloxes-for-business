aa2e927d55b93d1ba367033052947c93f9bcdc87
Ruan Ramalho <ruanmbramalho@gmail.com>
2021-09-09 20:27:35 -0300

    feat(header): add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import GlobalStyle from './theme/globalStyles';
+import Header from './components/Header';
 import News from './components/News';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <News />
     </>
   );
